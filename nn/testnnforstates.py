8255fc71226ee75668d33915d1d60b9b490b76c7
azconita <gabriela.azcona@despegar.com>
2017-11-21 19:53:20 -0300

    cambio test

--- nn/testnnforstates.py
+++ nn/testnnforstates.py
@@ -40,18 +40,19 @@ def test_states(epochs=5, trainsize=0.8, learning_rate=None):
             sh = trainprices.shape[0]
             trainprices = trainprices.reshape((1,sh))
 
-            parameters = nn_model(train.T, trainprices, 10,num_iterations=5000,
+            parameters = nn_model(train.T, trainprices, 10,num_iterations=100,
                                 parameters=parameters, learning_rate=learning_rate)
 
             predictions = predict(parameters, test.T, testprices.std(), testprices.mean())
             mse = np.square(testprices.T - predictions).mean()
             print("MSE: %s" % mse)
+        predict_test(g, parameters, prices.std(), prices.mean(),filename=str(g)+'submit.csv')
 
-        results[g] = {'parameters': parameters, 'pricestd': prices.std(), 'pricemean': prices.mean()}
+        #results[g] = {'parameters': parameters, 'pricestd': prices.std(), 'pricemean': prices.mean()}
 
     return results
 
-def predict_test(parameters, pricesstd, pricesmean, filename="submit.csv"):
+def predict_test(g, parameters, pricesstd, pricesmean, filename="submit.csv"):
     """
     For predicting de test set, saving a file for submition.
     Args:
@@ -65,27 +66,28 @@ def predict_test(parameters, pricesstd, pricesmean, filename="submit.csv"):
     states = dataset.groupby(["state_name"])
 
     results = {}
-    for g in states.groups.keys():
-        places = one_hot_encode(state[["place_name"]].values)
-        expenses = standarize(dataset[["expenses"]].values)
-        surfacetotal = standarize(dataset[["surface_total_in_m2"]].values)
-        surfacecovered = standarize(dataset[["surface_covered_in_m2"]].values)
-        proptypes = one_hot_encode(dataset[["property_type"]].values - 1)
-        xyz = coordenates_encode(dataset[["lat"]].values,dataset[["lon"]].values)
+    #for g in states.groups.keys():
+    state = states.get_group(g)
+    places = one_hot_encode(state[["place_name"]].values)
+    expenses = standarize(state[["expenses"]].values)
+    surfacetotal = standarize(state[["surface_total_in_m2"]].values)
+    surfacecovered = standarize(state[["surface_covered_in_m2"]].values)
+    proptypes = one_hot_encode(state[["property_type"]].values - 1)
+    xyz = coordenates_encode(state[["lat"]].values,state[["lon"]].values)
 
-        datastd = unite_subcols(expenses, surfacetotal)
-        datastd = unite_subcols(datastd, surfacecovered)
-        datastd = unite_subcols(datastd, proptypes)
-        datastd = unite_subcols(datastd, places)
-        datastd = unite_subcols(datastd, xyz)
+    datastd = unite_subcols(expenses, surfacetotal)
+    datastd = unite_subcols(datastd, surfacecovered)
+    datastd = unite_subcols(datastd, proptypes)
+    datastd = unite_subcols(datastd, places)
+    datastd = unite_subcols(datastd, xyz)
 
-        predictions = predict(parameters, datastd.T, pricesstd, pricesmean)
-        results = unite_subcols(ids, predictions.T)
-        np.savetxt(filename, results, fmt='%i', delimiter=",")
+    predictions = predict(parameters, datastd.T, pricesstd, pricesmean)
+    results = unite_subcols(ids, predictions.T)
+    np.savetxt(filename, results, fmt='%i', delimiter=",")
 
 if __name__ == '__main__':
-    results = test_states(learning_rate=0.45)
-    for g in results.keys():
-        predict_test(results[g]['parameters'],
-                     results[g]['pricestd'],
-                     results[g]['pricemean'],filename=str(g)+'submit.csv')
+    results = test_states(learning_rate=0.45, epochs=1)
+    #for g in results.keys():
+    #    predict_test(results[g]['parameters'],
+    #                 results[g]['pricestd'],
+    #                 results[g]['pricemean'],filename=str(g)+'submit.csv')
